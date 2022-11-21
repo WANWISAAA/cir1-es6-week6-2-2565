274c947710aa38bba30b33823bfb5d0395f33bf3
WANWISAAA <chumpornw6@gmail.com>
2022-11-21 09:57:15 +0700

    update

---
+++ week603.js
@@ -0,0 +1,4 @@
+const obj = {a:1,b:2,c:3}
+for(let prop in obj){
+    console.log(obj[prop])
+}
---
+++ week604.js
@@ -0,0 +1,5 @@
+let province = {a:"Trat",b:"Chantaburi",c:"23000"}
+
+for(let thai in province){
+    console.log(province[thai])
+}
